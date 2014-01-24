76026e373de2fc4d11084dd065a6fe231ff5290e
Austin deBruyn <austin.debruyn@yahoo.com>
2014-01-24 00:05:59 -0600

    front end skeleton

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+	require_once 'connect.php';
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<title>Media Poll</title>
+		<link href="theme.css" rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300' rel='stylesheet' type='text/css'>
+	</head>
+
+	<body>
